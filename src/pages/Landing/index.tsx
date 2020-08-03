e42a0b78e3608238d6308ba2d40cb73858538fcf
vinicfrancisco <vini.ibi@gmail.com>
2020-08-03 10:44:46 -0300

    added eslint and landing assets

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+
+export default function Landing() {
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="Proffy" />
+
+                    <h2>Sua plataforma de estudos online.</h2>
+                </div>
+
+                <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
+            </div>
+        </div>
+    )
+}
